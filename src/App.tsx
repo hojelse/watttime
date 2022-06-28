8fc807352d532b913b30d13695d752a39773a12d
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-06-28 21:14:08 +0200

    Update styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ query Dataset {
     order_by: {
       HourUTC: desc
     }
-    limit: 48
+    limit: 33
     offset: 0
   )
   {HourUTC HourDK PriceArea SpotPriceDKK SpotPriceEUR }
@@ -22,7 +22,8 @@ query Dataset {
 `
 
 function App() {
-  const [data, setBlah] = useState<DatasetQuery | undefined>(undefined)
+  const [dataset, setDataset] = useState<DatasetQuery | undefined>(undefined)
+  const [lastFetchTime, setLastFetchTime] = useState<Date | undefined>(undefined)
 
   useEffect(() => {
     const loader = async () => {  
@@ -35,13 +36,17 @@ function App() {
       return resp as DatasetQuery
     }
 
-    loader().then(x => setBlah(x))
+    loader().then(x => {
+      setDataset(x)
+      setLastFetchTime(new Date())
+    })
   }, [])
 
   return (
     <>
-      <Chart data={data}/>
-      <Table data={data}/>
+      <Chart data={dataset}/>
+      <p>Time of last data fetch: {lastFetchTime != undefined ? lastFetchTime.toUTCString() : "No data yet"}</p>
+      <Table data={dataset}/>
     </>
   )
 }
@@ -54,15 +59,9 @@ export function Table({ data } : { data: DatasetQuery | undefined }) {
   const pricesTransformed = prices.map(
     ({HourDK, SpotPriceEUR}) => {
 
-    const priceEur = SpotPriceEUR as number;
-    const moms = 1.25;
-    const eurToDkk = 7.5;
-    const mwtToKwt = 0.001;
-    const price = priceEur * moms * eurToDkk * mwtToKwt;
-
     return {
       date: new Date(HourDK),
-      price: price
+      price: tranformPrice(SpotPriceEUR)
     }
   })
 
@@ -71,7 +70,7 @@ export function Table({ data } : { data: DatasetQuery | undefined }) {
 
   return <table
       style={{
-        minWidth: "100%"
+        minWidth: "100vw"
       }}
     >
     <tr>
@@ -95,152 +94,192 @@ export function Table({ data } : { data: DatasetQuery | undefined }) {
 
 export function Chart({ data } : { data: DatasetQuery | undefined }) {
   if (data == undefined) return null
-
   var prices = data.elspotprices
-
-  const pricesTransformed = prices.map(
-    ({HourDK, SpotPriceEUR}) => {
-
-    const priceEur = SpotPriceEUR as number;
-    const moms = 1.25;
-    const eurToDkk = 7.5;
-    const mwtToKwt = 0.001;
-    const price = priceEur * moms * eurToDkk * mwtToKwt;
-
-    return {
-      date: new Date(HourDK),
-      price: price
-    }
-  })
-  .reverse()
+  const transformedPrices = transformPrices()
  
-  const [dataset, setDataset] = useState(pricesTransformed)
+  const [dataset, setDataset] = useState(transformedPrices)
+
+  const CANVAS_WIDTH = 4
+  const CANVAS_HEIGHT = 4
 
   const XMAX = dataset.length-1
-  const maxEntry = d3.max(dataset, (x) => x.price)
-  const YMAX = Math.round(maxEntry ?? 0) + 1
-  const BASELINE = YMAX
+  const maxPrice = d3.max(dataset, (x) => x.price)
+  const YMAX = Math.round(maxPrice ?? 0) + 1
 
-  const yaxis = d3.range(6)
+  const yaxis = d3.range(1, YMAX+1)
   
-  const scaleX = d3.scaleLinear()
+  const X = d3.scaleLinear()
     .domain([0, XMAX])
-    .range([0, 10])
+    .range([0, CANVAS_WIDTH])
 
-  let x = d3.scaleTime()
+  const x = d3.scaleTime()
     .domain([dataset[0].date, dataset[dataset.length-1].date])
     .range([ 0, XMAX ])
 
-  const stepCurve = dataset.reduce((prev, curr, idx) => {
-    return `${prev},${BASELINE - curr.price} l ${scaleX(1)},0 L ${scaleX(idx+1)}`
-  }, "M 0")
+  const Y = d3.scaleLinear()
+    .domain([0, YMAX])
+    .range([0, CANVAS_HEIGHT])
 
-  return (
-    <svg viewBox={`-1 -1 ${scaleX(XMAX)+2} ${YMAX+2}`} style={{height: "100vh"}}>
-      <Baseline
-        d={`M 0 ${BASELINE} L ${scaleX(XMAX+1)} ${BASELINE}`}
-      />
+  const BASELINE = Y(YMAX)
+
+
+  const stepCurve = createStepCurve()
+
+  return <>
+    <Canvas viewBox={`-0.5 -0.5 ${X(XMAX)+1} ${Y(YMAX)+1}`} >
       {
         yaxis.map(y => {
-          return (
-            <>
-              <text
-                x={0}
-                y={BASELINE-y}
-                font-size="0.006em"
-                textAnchor="end"
-                dominantBaseline="central"
-                dx="-1em"
-              >
-                {`${y} DKK/KWh`}
-              </text>
-              <path
-                d={`M 0 ${BASELINE-y} l ${scaleX(XMAX+1)} 0`}
-                fill="none"
-                stroke="hsl(0, 0%, 20%)"
-                strokeWidth="var(--chart-divider-width)"
-              />
-            </>
-          )
+          return <>
+              <Price x={0} y={BASELINE-Y(y)} dx="-1em" >
+                {`${y}`}
+              </Price>
+              <ChartDividerY d={`M 0 ${BASELINE-Y(y)} l ${X(XMAX+1)} 0`} />
+          </>
         })
       }
+      {
+        dataset.map(({date, price}, idx) => {
+          const before = dataset[idx-1]?.price
+          const after = dataset[idx+1]?.price
+          const isStart = (idx == 0)
+          const isEnd = (idx == dataset.length-1)
+          const isLocalMax = (before < price && price > after) || (isEnd && before < price)
+          const isLocalMin = (before > price && price < after) || (isEnd && before > price)
+          const isLocalM = isLocalMax || isLocalMin
+          const is00 = date.getHours() == 0
+          return <>
+            <ChartDividerX
+              d={`M ${X(x(date))} ${BASELINE-Y(price)} L ${X(x(date))} ${BASELINE}`}
+            />
+            {
+              (isLocalMin )
+              ? <path
+                  d={`M ${X(x(date))},${BASELINE-Y(price)} L${X(x(date)+1)},${BASELINE-Y(price)} L${X(x(date)+1)},${BASELINE} L${X(x(date))},${BASELINE}`}
+                  fill="hsla(var(--color-hue-primary), 50%, 80%, 0.9)"
+                />
+              : null
+            }
+            <DateHour x={X(x(date)+0.5)} y={BASELINE} dy="1em" >
+              {`${date.toLocaleString("da-DK", { hour: "2-digit" })}`}
+            </DateHour>
+            {
+              (is00 || isStart)
+              ? <DateWeekday x={X(x(date))} y={BASELINE} dy="3em" >
+                  {`${date.toLocaleString("da-DK", { weekday: "long" })}`}
+                </DateWeekday>
+              : null
+            }
+            {
+              (isLocalM || isEnd)
+              ? <ColumnText x={X(x(date)+0.5)} y={BASELINE-Y(price)} dy="-2em" >
+                  {Math.round(price*100)/100}
+                </ColumnText>
+              : null
+            }
+          </>
+        })
+      }
+      <Gradient d={`${stepCurve} ${BASELINE} L ${0} ${BASELINE} z`} />
+      <Stepcurve d={stepCurve} />
+      <Baseline d={`M 0 ${BASELINE} L ${X(XMAX+1)} ${BASELINE}`} />
       <defs>
-        <linearGradient
-          id="Gradient1"
-          x1={0}
-          x2={0}
-          y1={0}
-          y2={1}
-        >
+        <linearGradient id="Gradient1" x1={0} x2={0} y1={0} y2={1} >
           <stop offset="0%" stopColor="hsla(var(--color-hue-primary), 50%, 50%, 0.6)"/>
           <stop offset="100%" stopColor="hsla(var(--color-hue-primary), 50%, 50%, 0.1)"/>
         </linearGradient>
       </defs>
-      {
-        dataset.map(({date, price}) => {
-          return (
-            <>
-              <path
-                d={`M ${scaleX(x(date))} ${BASELINE-price} L ${scaleX(x(date))} ${BASELINE}`}
-                fill="none"
-                stroke="hsla(var(--color-hue-primary), 100%, 40%, 1)"
-                strokeWidth="var(--chart-divider-width)"
-              />
-              <text
-                x={scaleX(x(date))}
-                y={BASELINE}
-                font-size="0.006em"
-                textAnchor="middle"
-                dominantBaseline="hanging"
-                dy="1em"
-              >
-                {`${date.toLocaleString("da-DK", { hour: "2-digit" })}h`}
-              </text>
-              <text
-                x={scaleX(x(date))}
-                y={BASELINE}
-                font-size="0.006em"
-                textAnchor="middle"
-                dominantBaseline="hanging"
-                dy="3em"
-              >
-                {`${date.toLocaleString("da-DK", { weekday: "short" })}`}
-              </text>
-              <text
-                x={scaleX(x(date)+0.5)}
-                y={BASELINE-price}
-                font-size="0.006em"
-                textAnchor="middle"
-                fontWeight="bold"
-                fill="hsl(var(--color-hue-primary), 100%, 30%)"
-                dy="-2em"
-              >
-                {Math.round(price*100)/100}
-              </text>
-            </>
-          )
-        })
-      }
-      <path
-        d={`${stepCurve} ${BASELINE} L ${0} ${BASELINE} z`}
-        fill="url(#Gradient1)"
-      />
-      <path
-        d={stepCurve}
-        fill="none"
-        stroke="hsla(var(--color-hue-primary), 100%, 40%, 1)"
-        strokeWidth="var(--chart-border-width)"
-      />
-    </svg>
-  )
+    </Canvas>
+  </>
+
+  function transformPrices() {
+    return prices.map(
+      ({ HourDK, SpotPriceEUR }) => {
+        return {
+          date: new Date(HourDK),
+          price: tranformPrice(SpotPriceEUR)
+        }
+      })
+      .reverse()
+  }
+
+  function createStepCurve() {
+    return dataset.reduce((prev, curr, idx) => {
+      return `${prev},${BASELINE - Y(curr.price)} l ${X(1)},0 L ${X(idx + 1)}`
+    }, "M 0")
+  }
 }
 
+const Canvas = styled.svg`
+  min-width: 100vw;
+  height: 100vh;
+`
+
+const Gradient = styled.path`
+  fill: url(#Gradient1);
+`
+
+const ChartDividerY = styled.path`
+  fill: none;
+  stroke: var(--color-foreground);
+  stroke-width: var(--chart-divider-width);
+  stroke-linecap: round;
+`
+const ChartDividerX = styled.path`
+  fill: none;
+  stroke: hsla(var(--color-hue-primary), 100%, 40%, 1);
+  stroke-width: var(--chart-divider-width);
+  stroke-linecap: round;
+`
+
+const Price = styled.text`
+  fill: var(--color-foreground);
+  text-anchor: end;
+  dominant-baseline: central;
+  font-size: 0.006em;
+`
+
+const DateHour = styled.text`
+  fill: var(--color-foreground);
+  text-anchor: middle;
+  dominant-baseline: hanging;
+  font-size: 0.006em;
+`
+
+const DateWeekday = styled.text`
+  fill: var(--color-foreground);
+  text-anchor: start;
+  dominant-baseline: hanging;
+  font-size: 0.006em;
+`
+
+const ColumnText = styled.text`
+  font-size: 0.006em;
+  text-anchor: middle;
+  font-weight: 500;
+  fill: hsl(var(--color-hue-primary), 100%, 30%);
+`
+
+const Stepcurve = styled.path`
+  fill: none;
+  stroke: hsla(var(--color-hue-primary), 100%, 40%, 1);
+  stroke-width: var(--chart-border-width);
+  stroke-linecap: round;
+`
 
 const Baseline = styled.path`
   fill: none;
-  stroke: hsl(0, 0%, 20%);
+  stroke: var(--color-foreground);
   stroke-width: var(--chart-border-width);
   stroke-linecap: round;
 `
 
 export default App
+
+function tranformPrice(SpotPriceEUR: any) {
+  const priceEur = SpotPriceEUR as number
+  const moms = 1.25
+  const eurToDkk = 7.5
+  const mwtToKwt = 0.001
+  const price = priceEur * moms * eurToDkk * mwtToKwt
+  return price
+}
