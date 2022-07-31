ede3bb56e16696bee85aa54279c2ca5fe17b76f9
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-07-31 13:32:38 +0200

    Change meta theme colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           sizes="32x32"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="hsl(0, 0%, 5%)" />
       </Head>
       <Component {...pageProps} />
   </>
