a22f239667b18fbbe5f239dde45531914b56202b
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2023-07-28 22:25:45 +0200

    Update pwa config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 module.exports = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+  // next.js config
+})
