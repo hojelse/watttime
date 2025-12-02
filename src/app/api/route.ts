c9d72dfa970b82c208c79df0a7b2255871aaa912
nimrossum <1959615+nimrossum@users.noreply.github.com>
2025-12-02 09:21:14 +0100

    Change GET response to use NextResponse

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,4 @@
+import { NextResponse } from 'next/server'
 import { getData } from "./get_data"
 
-export const GET = async () => Response.json(await getData())
-
+export const GET = async () => NextResponse.json(await getData())
