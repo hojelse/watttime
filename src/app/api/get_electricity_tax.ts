24e649a5a33a8fc7ffeb11969ac99428b6ce31c2
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2025-12-27 14:04:10 +0100

    Modify electricity tax rates and add source comments
    Updated electricity tax rates and added sources for reference.

--- src/app/api/get_electricity_tax.ts
+++ src/app/api/get_electricity_tax.ts
@@ -1,17 +1,12 @@
-// source: www.elbiil.dk/opladning/aendring-i-elafgiften/
 export function getElectricityTax(date: Date) {
   if (date < new Date("2022-01-01T00:00:00")) return 0
-  if (date < new Date("2022-06-01T00:00:00")) return 90.30
+  if (date < new Date("2022-06-01T00:00:00")) return 90.30 // source: www.elbiil.dk/opladning/aendring-i-elafgiften/
   if (date < new Date("2022-09-01T00:00:00")) return 76.30
   if (date < new Date("2023-01-01T00:00:00")) return 72.30
   if (date < new Date("2023-07-01T00:00:00")) return 0.80
   if (date < new Date("2024-01-01T00:00:00")) return 69.70
   if (date < new Date("2025-01-01T00:00:00")) return 71.00
-  if (date < new Date("2026-01-01T00:00:00")) return 64.80
-  if (date < new Date("2027-01-01T00:00:00")) return 64.80
-  if (date < new Date("2028-01-01T00:00:00")) return 64.80
-  if (date < new Date("2029-01-01T00:00:00")) return 63.20
-  if (date < new Date("2030-01-01T00:00:00")) return 61.70
-  if (date < new Date("2031-01-01T00:00:00")) return 56.10
+  if (date < new Date("2026-01-01T00:00:00")) return 72.00 
+  if (date < new Date("2028-01-01T00:00:00")) return 00.80 // source: https://skm.dk/media/fg2pvujk/faktaark-om-lempelse-af-elafgift.pdf
   return 0
 }
