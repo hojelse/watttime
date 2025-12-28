b1c01c0e5c95c62414b365b3ff3a5b2d3b8dff79
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2025-12-28 02:01:05 +0100

    Fix incorrect return value for electricity tax

--- src/app/api/get_electricity_tax.ts
+++ src/app/api/get_electricity_tax.ts
@@ -7,6 +7,6 @@ export function getElectricityTax(date: Date) {
   if (date < new Date("2024-01-01T00:00:00")) return 69.70
   if (date < new Date("2025-01-01T00:00:00")) return 71.00
   if (date < new Date("2026-01-01T00:00:00")) return 72.00 
-  if (date < new Date("2028-01-01T00:00:00")) return 00.80 // source: https://skm.dk/media/fg2pvujk/faktaark-om-lempelse-af-elafgift.pdf
+  if (date < new Date("2028-01-01T00:00:00")) return 0.80  // source: https://skm.dk/media/fg2pvujk/faktaark-om-lempelse-af-elafgift.pdf
   return 0
 }
